6e159ded95120ab050947ef3b01a8e93d1ee8e27
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-09-18 16:53:53 +0200

    Add files via upload

---
+++ boilerplate.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef pair<int, int> II;
+typedef vector<II> VII;
+
+#define REP(x, n) for (int x = 0; x < (n); ++x)
+#define INF (1e9 + 1)
+#define EPS 1e-9
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+
+int main() {
+    /*
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    */
+
+    return 0;
+}
