2232ce5cdbc712f035c6c2c07c4b69fd8025d413
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-09-21 17:47:57 +0200

    Update boilerplate.cpp

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -7,6 +7,7 @@ typedef pair<int, int> II;
 typedef vector<II> VII;
 
 #define REP(x, n) for (int x = 0; x < (n); ++x)
+#define IT(s) s.begin(), s.end()
 #define INF (1e9 + 1)
 #define EPS 1e-9
 #define PB push_back
